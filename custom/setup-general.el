74ab638eb12fe1e4f42691e69c2e853d0ef0184d
amit barve <barvea@vmware.com>
2018-03-24 23:56:36 -0700

    added few convenience configurations: show line numbers, highlight current line etc

--- custom/setup-general.el
+++ custom/setup-general.el
@@ -20,6 +20,27 @@
 ;; activate whitespace-mode to view all whitespace characters
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 
+;; load theme
 (load-theme 'wombat)
+
+;; don't create backup files
+(setq create-lockfiles nil)
+
+;; no blinking cursor
+(blink-cursor-mode 0)
+
+
+;; line numbers
+(global-linum-mode t)
+
+;; short answers to yes/no questions
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; highlight current line
+(global-hl-line-mode 1)
+
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (provide 'setup-general)
